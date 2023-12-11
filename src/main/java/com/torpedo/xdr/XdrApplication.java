4fd7eb68dd23af8346bc214dbf5bd172469897ff
Janusz Dlugosz <jandlu08@gmail.com>
2023-12-11 09:16:21 +0100

    Initial commit

---
+++ src/main/java/com/torpedo/xdr/XdrApplication.java
@@ -0,0 +1,13 @@
+package com.torpedo.xdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XdrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torpedo/xdr/XdrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torpedo.xdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
